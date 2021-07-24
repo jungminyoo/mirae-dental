51c49c6797b96b556b44a6b2425f87ca4c3c51d7
jungminyoo <yoojm4718@gmail.com>
2021-07-24 13:59:31 +0900

    Installed dotenv and morgan

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import "dotenv/config";
 
-const PORT = 4000;
 const app = express();
+const PORT = 4000;
+const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(logger);
 
 const handleListening = () => console.log(`Server listening on port ${PORT}`);
 
