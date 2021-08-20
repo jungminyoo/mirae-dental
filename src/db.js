abab8d83fc2f17774e0626fb48f3117b0d01ba00
jungminyoo <yoojm4718@gmail.com>
2021-08-20 15:05:14 +0900

    Added error argument on db.js

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/mirae", {
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("Connected to DB!");
-const handleError = () => console.log("DB Error!", error);
+const handleError = (error) => console.log("DB Error!", error);
 
 db.on("error", handleError);
 db.once("open", handleOpen);
